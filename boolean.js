1fbc7c544f3f4bf93227639add0e2b1c47d791e4
Your Name <your.email@example.com>
2025-09-26 17:02:08 +0300

    add:решение двух математических выражений

---
+++ boolean.js
@@ -0,0 +1,4 @@
+const MAGIC_NUMBER=28**8-306
+const MAGIC_MOD_VALUE=10*3%8
+const result=MAGIC_NUMBER<MAGIC_MOD_VALUE
+console.log(result)
---
+++ var.js
@@ -0,0 +1,4 @@
+const firstNumber=18
+const secondNumber=13
+const result= firstNumber + secondNumber
+console.log(result)
